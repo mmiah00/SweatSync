dfaa97cb81fd74f648c7053b6b8ba3b820546c7e
mmiah <mmiah@bu.edu>
2023-10-21 16:35:42 -0400

    editing info card for gym list

--- SweatSync/App.js
+++ SweatSync/App.js
@@ -3,17 +3,54 @@ import { StyleSheet, Text, View } from 'react-native';
 import GymInfo from "./components/Gyminfo"; 
 
 export default function App() {
+  const gyms = [
+    {
+      name: "Gym A",
+      address: "123 Main St",
+      activities: ["Yoga", "Pilates"]
+    },
+    {
+      name: "Gym B", 
+      address: "456 Park Ave",
+      activities: ["Basketball", "Boxing"]
+    }
+  ]; 
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
-      <GymInfo 
-        name="ABC Gym"
-        address="123 Main St, Anytown USA"
-        activities={['Weight Lifting', 'Rock Climbing', 'Yoga']}
-      />
+      {renderGymCards(gyms)}
     </View>
   );
+}
+
+function renderGymCards(gyms) {
+  return gyms.map(gym => (
+    <GymInfo 
+      key={gym.name}
+      name={gym.name} 
+      address={gym.address}
+      activities={gym.activities}
+    />
+  ));
+}
+
+export const getCurrentLocation = (simulator) => {
+  return async (dispatch) => {
+    if (!simulator) {
+      let { status } = await Permissions.askAsync(Permissions.LOCATION);
+
+      if (status !== 'granted') {
+        dispatch(gotCurrentLocationError('Permission to access location was denied'))
+      }
+
+      let location = await Location.getCurrentPositionAsync({});
+      dispatch(gotCurrentLocation(location))
+    } else {
+      // Demo location for simulator
+      dispatch(gotCurrentLocation(chicagoFSA))
+    }
+  }
 }
 
 const styles = StyleSheet.create({
--- SweatSync/components/Gyminfo.js
+++ SweatSync/components/Gyminfo.js
@@ -27,7 +27,8 @@ const styles = StyleSheet.create({
     shadowOffset: { width: 0, height: 2 },
     shadowOpacity: 0.8,
     shadowRadius: 2,
-    elevation: 1
+    elevation: 1, 
+    width: "50%"
   },
   name: {
     fontSize: 20,
