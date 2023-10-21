365e630d733c018632dc9767372d1ffb33c8d4b8
mmiah <mmiah@bu.edu>
2023-10-21 12:09:27 -0400

    empty folder for components

---
+++ SweatSync/components/Gyminfo.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import {Text} from 'react-native';
+
+export default function gyminfo () { 
+    return (
+        <div> 
+            <Text> Central Rock Gym </Text>
+            <Text> 20 Brattle St, Cambridge, MA 02138 </Text>
+        </div> 
+    )
+}
