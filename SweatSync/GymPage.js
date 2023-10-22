63d62cf81d22d9d89cd4abe028d59d48e166ae17
mmiah <mmiah@bu.edu>
2023-10-22 05:02:01 -0400

    edited rendering of posts

--- SweatSync/GymPage.js
+++ SweatSync/GymPage.js
@@ -1,9 +1,19 @@
 import React, {useState, useEffect} from "react";
 import { StyleSheet, Text, View } from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
 
 
 export default function GymPage (props) { 
-    const posts = props.posts; 
+    const [posts, setPosts] = useState([]);
+
+    useEffect(() => {
+        const getPosts = async () => {
+        const posts = await AsyncStorage.getItem('posts');
+        setPosts(posts); 
+        }
+        getPosts();
+    }, []); 
 
     return ( 
         <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
