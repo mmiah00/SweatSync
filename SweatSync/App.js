1373f84c875b5c8d6bc90b899fa6df0c40fb44d8
arufak <arufamanar1@gmail.com>
2023-10-21 20:20:04 -0400

    working on UI and css

--- SweatSync/App.js
+++ SweatSync/App.js
@@ -1,20 +1,23 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import "./style.css";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export const HomeScreen = () => {
+    return (
+        <div className="home-screen">
+            <div className="overlap-group-wrapper">
+                <div className="overlap-group">
+                    <div className="streamline-interface" />
+                    <img className="screenshot" alt="Screenshot" src="screenshot-2023-10-21-121613-1.png" />
+                    <img className="vector" alt="Vector" src="vector.svg" />
+                    <p className="any-plans-for-the">
+                        <span className="text-wrapper">Any plans for the day?</span>
+                        <span className="span">&nbsp;</span>
+                    </p>
+                    <img className="subway-up" alt="Subway up" src="subway-up-2.svg" />
+                    <img className="ph-map-pin-fill" alt="Ph map pin fill" src="ph-map-pin-fill.svg" />
+                    <img className="img" alt="Ph map pin fill" src="image.svg" />
+                </div>
+            </div>
+        </div>
+    );
+};
