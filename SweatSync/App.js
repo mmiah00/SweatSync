1c04196cd0e5160da48b22d72178b5673bcec847
arien97 <aramin@bu.edu>
2023-10-22 04:38:47 -0400

    made updates to App.js

--- SweatSync/App.js
+++ SweatSync/App.js
@@ -2,69 +2,29 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import Sidebar from './components/Sidebar';
 import GymInfo from "./components/Gyminfo";
-import React from "react";
+import React, {useState, useEffect} from "react";
 import "./style.css";
 import Map from "./components/Map";
-import { GoogleSignin, GoogleSigninButton } from '@react-native-google-signin/google-signin';  
+import { NavigationContainer } from '@react-navigation/native';
+import {
+  createDrawerNavigator,
+  DrawerContentScrollView,
+  DrawerItemList,
+  DrawerItem,
+} from '@react-navigation/drawer';
+
 
 
 
 export default function App() {
-  const [user, setUser] = useState(null);
-
-  const signIn = async () => {
-
-    try {
-      await GoogleSignin.hasPlayServices();
-      const userInfo = await GoogleSignin.signIn();
-      setUser(userInfo); // update user state
-      
-    } catch (error) {
-      console.log(error);
-    }
   
-  };
-
-  const gyms = [
-    {
-      name: "Gym A",
-      address: "123 Main St",
-      activities: ["Yoga", "Pilates"]
-    },
-    {
-      name: "Gym B",
-      address: "456 Park Ave",
-      activities: ["Basketball", "Boxing"]
-    }
-  ];
-
-  return (
-    <View style={styles.container}>
-
-      {user ? (
-        // Show app content
-        <> 
-          <Sidebar />
-          <StatusBar style="auto" />
-          {renderGymCards(gyms)}
-        </>
-      ) : (  
-        // Show Google sign-in button
-        <GoogleSigninButton
-          onPress={signIn}
-        />
-      )}
-
-    </View>
+return (
+    <NavigationContainer>
+      <MyDrawer />
+    </NavigationContainer>
   );
 
 };
-
-GoogleSignin.configure({
-  webClientId: '708515035791-pinpdso572m43l1s0c51uphd84s42pq5.apps.googleusercontent.com',
-  offlineAccess: true
-});
-
 
 function renderGymCards(gyms) {
   return gyms.map(gym => (
@@ -77,6 +37,99 @@ function renderGymCards(gyms) {
   ));
 }
 
+const gyms = [
+    {
+      name: "Gym A",
+      address: "123 Main St",
+      activities: ["Yoga", "Pilates"]
+    },
+    {
+      name: "Gym B",
+      address: "456 Park Ave",
+      activities: ["Basketball", "Boxing"]
+    }
+  ];
+
+function Home({ navigation }) {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <React.Fragment>
+        <Map />
+        {renderGymCards(gyms)}
+      </React.Fragment>
+    </View>
+  );
+}
+
+
+function Feed() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Feed</Text>
+    </View>
+  );
+}
+
+
+function Profile() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+      <Text>Profile </Text>
+    </View>
+  )
+}
+
+
+function ConnectWearable() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+      <Text>Connect Wearable </Text>
+    </View>
+  )
+}
+
+
+function Settings() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+      <Text>Settings </Text>
+    </View>
+  )
+}
+
+
+function CustomDrawerContent(props) {
+  return (
+    <DrawerContentScrollView {...props}>
+      <DrawerItemList {...props} />
+      <DrawerItem
+        label="Sign Out"
+        onPress={() => props.navigation.closeDrawer()}
+      />
+    </DrawerContentScrollView>
+  );
+}
+
+
+const Drawer = createDrawerNavigator();
+
+
+function MyDrawer() {
+  return (
+    <Drawer.Navigator
+      useLegacyImplementation
+      drawerContent={(props) => <CustomDrawerContent {...props} />}
+    >
+      <Drawer.Screen name="Home" component={Home} />
+      <Drawer.Screen name="Feed" component={Feed} />
+      <Drawer.Screen name="Profile" component={Profile} />
+      <Drawer.Screen name="Connect Wearable" component={ConnectWearable} />
+      <Drawer.Screen name="Settings" component={Settings} />
+    </Drawer.Navigator>
+  );
+}
+
+
 
 export const getCurrentLocation = (simulator) => {
   return async (dispatch) => {
@@ -84,9 +137,13 @@ export const getCurrentLocation = (simulator) => {
       let { status } = await Permissions.askAsync(Permissions.LOCATION);
 
 
+
+
       if (status !== 'granted') {
         dispatch(gotCurrentLocationError('Permission to access location was denied'))
       }
+
+
 
 
       let location = await Location.getCurrentPositionAsync({});
@@ -99,6 +156,8 @@ export const getCurrentLocation = (simulator) => {
 }
 
 
+
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -107,6 +166,8 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+
 
 
 export const HomeScreen = () => {
@@ -129,3 +190,6 @@ export const HomeScreen = () => {
         </div>
     );
 };
+
+
+
